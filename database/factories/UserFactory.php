1d5a6e5310cd4403d7a8d49b2addfb1ee4137987
mehranDashti <mehran19.md@gmail.com>
2020-12-17 22:49:46 +0330

    The user model and factory has been change according to user table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,9 +4,11 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+
     /**
      * The name of the factory's corresponding model.
      *
@@ -24,6 +26,9 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => time(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
         ];
     }
 }
